f4c88de11579572731216237b594cb3e20ab1c44
Rogerio Assiz <rogerio.assiz@uol.com.br>
2019-02-18 15:16:08 -0300

    Obtendo um usuário por id

--- src/main/java/com/assiz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/assiz/workshopmongo/resources/UserResource.java
@@ -1,10 +1,13 @@
 package com.assiz.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,14 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 		
 	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+		
+	}
+	
+	
 
 }
--- src/main/java/com/assiz/workshopmongo/services/UserService.java
+++ src/main/java/com/assiz/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.assiz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.assiz.workshopmongo.domain.User;
 import com.assiz.workshopmongo.repository.UserRepository;
+import com.assiz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
 
 }
