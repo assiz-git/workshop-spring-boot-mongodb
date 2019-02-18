623d64b4871192e22b651fdf36964f9dc30e9219
Rogerio Assiz <rogerio.assiz@uol.com.br>
2019-02-18 11:40:07 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/assiz/workshopmongo/services/UserService.java
+++ src/main/java/com/assiz/workshopmongo/services/UserService.java
@@ -3,7 +3,6 @@ package com.assiz.workshopmongo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
 import com.assiz.workshopmongo.domain.User;
@@ -16,7 +15,7 @@ public class UserService {
 	private UserRepository userRepository;
 	
 	public List<User> findAll() {
-		return ((MongoRepository<User, String>) userRepository).findAll();
+		return userRepository.findAll();
 	}
 
 }
