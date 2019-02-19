1327b33cc4e997b55b494849957664ad8e5b1449
Rogerio Assiz <rogerio.assiz@uol.com.br>
2019-02-19 15:29:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/assiz/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.assiz.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.assiz.workshopmongo.domain.Post;
+import com.assiz.workshopmongo.repository.PostRepository;
+import com.assiz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository PostRepository;
+
+	public List<Post> findAll() {
+		return PostRepository.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> Post = PostRepository.findById(id);
+		return Post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+	
+}
